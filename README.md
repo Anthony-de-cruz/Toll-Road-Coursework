3fec7ddaab162293b8eca41012ba03c1115396e3
Anthony de Cruz <anthonydecruz@proton.me>
2023-03-03 18:33:00 +0000

    Initialised project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Toll-Road-Coursework
+Programming 1 coursework no. 2
+
+Scored: - / 100
---
+++ src/main/java/TollRoadMain.java
@@ -0,0 +1,12 @@
+package main.java;
+
+/**
+ * @author Anthony de Cruz
+ */
+public class TollRoadMain {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
